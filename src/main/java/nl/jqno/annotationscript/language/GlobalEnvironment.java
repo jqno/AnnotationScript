8650edc1df46d20ef8dd4a03358773a8e8f8c09d
Jan Ouwens <jan.ouwens@gmail.com>
2020-06-21 12:58:45 +0200

    Adds a (very basic) GlobalEnvironment

---
+++ src/main/java/nl/jqno/annotationscript/language/GlobalEnvironment.java
@@ -0,0 +1,29 @@
+package nl.jqno.annotationscript.language;
+
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
+import io.vavr.collection.List;
+import nl.jqno.annotationscript.language.ast.*;
+
+public final class GlobalEnvironment {
+    private static final List<Tuple2<String, Fn>> GLOBAL = List.of(
+        Tuple.of("+", 
+            new Fn("+", Fn.VARARG, params -> new AstFloat(params.foldLeft(0.0, (acc, curr) -> acc + curr.asFloat())))),
+        Tuple.of("-", 
+            new Fn("-", Fn.VARARG, params -> new AstFloat(params.tail().foldLeft(params.head().asFloat(), (acc, curr) -> acc - curr.asFloat())))),
+        Tuple.of("*", 
+            new Fn("*", Fn.VARARG, params -> new AstFloat(params.foldLeft(1.0, (acc, curr) -> acc * curr.asFloat())))),
+        Tuple.of("/", 
+            new Fn("/", Fn.VARARG, params -> new AstFloat(params.tail().foldLeft(params.head().asFloat(), (acc, curr) -> acc / curr.asFloat())))),
+        Tuple.of("begin", 
+            new Fn("begin", Fn.VARARG, params -> params.last())),
+        Tuple.of("pi", 
+            new Fn("pi", 0, params -> new AstFloat(Math.PI)))
+    );
+
+    public static Environment build() {
+        return new Environment(GLOBAL.toMap(t -> t));
+    }
+
+    private GlobalEnvironment() {}
+}
---
+++ src/test/java/nl/jqno/annotationscript/language/GlobalEnvironmentTest.java
@@ -0,0 +1,52 @@
+package nl.jqno.annotationscript.language;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import io.vavr.collection.List;
+import nl.jqno.annotationscript.language.ast.*;
+
+public class GlobalEnvironmentTest {
+    private static final Environment ENV = GlobalEnvironment.build();
+
+    @Test
+    public void addition() {
+        assertEquals(new AstFloat(1), evaluate("+", new AstInt(1)));
+        assertEquals(new AstFloat(42), evaluate("+", new AstInt(3), new AstInt(33), new AstInt(6)));
+    }
+
+    @Test
+    public void subtraction() {
+        assertEquals(new AstFloat(1), evaluate("-", new AstInt(1)));
+        assertEquals(new AstFloat(10), evaluate("-", new AstInt(42), new AstInt(30), new AstInt(2)));
+    }
+
+    @Test
+    public void multiplication() {
+        assertEquals(new AstFloat(1), evaluate("*", new AstInt(1)));
+        assertEquals(new AstFloat(24), evaluate("*", new AstInt(2), new AstInt(3), new AstInt(4)));
+    }
+
+    @Test
+    public void division() {
+        assertEquals(new AstFloat(1), evaluate("/", new AstInt(1)));
+        assertEquals(new AstFloat(2.5), evaluate("/", new AstInt(10), new AstInt(2), new AstInt(2)));
+    }
+
+    @Test
+    public void begin() {
+        assertEquals(new AstInt(1), evaluate("begin", new AstInt(1)));
+        assertEquals(new AstSymbol("z"), evaluate("begin", new AstSymbol("x"), new AstSymbol("y"), new AstSymbol("z")));
+    }
+
+    @Test
+    public void pi() {
+        assertEquals(new AstFloat(Math.PI), evaluate("pi"));
+    }
+
+    private AstAtom evaluate(String symbol, AstAtom... params) {
+        var sut = ENV.lookup(symbol);
+        return sut.evaluate(List.of(params));
+    }
+}
