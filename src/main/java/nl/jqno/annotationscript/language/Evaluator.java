522ca9ba4d2eb00da3891459c9d7433a2454cad8
Jan Ouwens <jan.ouwens@gmail.com>
2020-07-05 12:23:28 +0200

    Clears Evaluator for re-writing

--- src/main/java/nl/jqno/annotationscript/language/Evaluator.java
+++ src/main/java/nl/jqno/annotationscript/language/Evaluator.java
@@ -1,75 +1,9 @@
 package nl.jqno.annotationscript.language;
 
-import io.vavr.Tuple;
-import io.vavr.Tuple2;
-import io.vavr.collection.List;
 import nl.jqno.annotationscript.language.ast.*;
 
 public class Evaluator {
-    public AstExp eval(AstExp x, Environment env) {
-        return evaluate(x, env, true)._1;
-    }
-
-    private Tuple2<AstExp, Environment> evaluate(AstExp x, Environment env, boolean followSymbols) {
-        if (x instanceof AstList) {
-            return evaluateList((AstList)x, env, followSymbols);
-        }
-        if (followSymbols && x instanceof AstSymbol) {
-            var symbol = (AstSymbol)x;
-            return Tuple.of(env.lookup(symbol.asSymbol()).evaluate(List.of()), env);
-        }
-
-        return Tuple.of(x, env);
-    }
-
-    private Tuple2<AstExp, Environment> evaluateList(AstList x, Environment env, boolean followSymbols) {
-        var list = x.value();
-        if (list.size() == 0) {
-            return Tuple.of(x, env);
-        }
-        var head = list.head();
-        if (head instanceof AstList) {
-            var procHead = evaluate(head, env, false);
-            return evaluateProc(procHead._1, list.tail(), procHead._2, followSymbols);
-        }
-        switch (head.asSymbol()) {
-            case "if":
-                var test = evaluate(list.get(1), env, followSymbols)._1;
-                var isFalse = test.value().equals(0);
-                var branch = isFalse ? list.get(3) : list.get(2);
-                return Tuple.of(evaluate(branch, env, followSymbols)._1, env);
-            case "define":
-                var symbol = list.get(1).asSymbol();
-                var exp = evaluate(list.get(2), env, followSymbols)._1;
-                if (exp instanceof AstLambda) {
-                    var lambda = (AstLambda)exp;
-                    var fn = lambda.asFn(this, env);
-                    return Tuple.of(lambda, env.add(symbol, fn));
-                }
-                return Tuple.of(exp, env.add(symbol, Fn.value(exp)));
-            case "lambda":
-                var params = ((AstList)list.get(1)).value().map(v -> (AstSymbol)v);
-                var body = list.get(2);
-                return Tuple.of(new AstLambda(params, body, env), env);
-            case "quote":
-                return Tuple.of(list.get(1), env);
-            default:
-                return evaluateProc(head, list.tail(), env, followSymbols);
-        }
-    }
-
-    private Tuple2<AstExp, Environment> evaluateProc(AstExp head, List<AstExp> tail, Environment env, boolean followSymbols) {
-        var fn = head instanceof AstLambda ? ((AstLambda)head).asFn(this, env) : env.lookup(head.asSymbol());
-        var params = tail
-            .foldLeft(
-                Tuple.<List<AstExp>, Environment>of(List.empty(), env),
-                (acc, curr) -> {
-                    var accList = acc._1;
-                    var accEnv = acc._2;
-                    var result = evaluate(curr, accEnv, followSymbols);
-                    return Tuple.of(accList.append(result._1), result._2);
-                })
-            ._1;
-        return Tuple.of(fn.evaluate(params), env);
+    public Object eval(AstExp x, Environment env) {
+        return null;
     }
 }
