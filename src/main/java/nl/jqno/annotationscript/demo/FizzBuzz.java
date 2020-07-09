30b26323b75584a5fe40e6a2a6e9c5ca23184efe
Jan Ouwens <jan.ouwens@gmail.com>
2020-07-09 16:07:20 +0200

    Adds FizzBuzz demo

---
+++ src/main/java/nl/jqno/annotationscript/demo/FizzBuzz.java
@@ -0,0 +1,19 @@
+package nl.jqno.annotationscript.demo;
+
+import nl.jqno.annotationscript.AnnotationScript;
+import nl.jqno.annotationscript.Annotations.*;
+
+@Zero("begin")
+@Zero(list={@One("define"), @One("fizz-buzz"), @One(list={@Two("lambda"), @Two(list=@Three("n")), @Two(list={
+    @Three("cond"),
+    @Three(list={@Four("="), @Four(list={@Five("%"), @Five("n"), @Five("15")}), @Four("0")}), @Three("'fizzbuzz'"),
+    @Three(list={@Four("="), @Four(list={@Five("%"), @Five("n"), @Five("3")}), @Four("0")}), @Three("'fizz'"),
+    @Three(list={@Four("="), @Four(list={@Five("%"), @Five("n"), @Five("5")}), @Four("0")}), @Three("'buzz'"),
+    @Three("1"), @Three("n")
+})})})
+@Zero(list={@One("map"), @One("println"), @One(list={@Two("map"), @Two("fizz-buzz"), @Two(list={@Three("range"), @Three("1"), @Three("101")})})})
+public class FizzBuzz {
+    public static void main(String[] args) {
+        AnnotationScript.run(FizzBuzz.class);
+    }
+}
