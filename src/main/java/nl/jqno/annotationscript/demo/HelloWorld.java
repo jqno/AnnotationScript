72501dc1186427d632730f7cb3bc8b8bf99aab82
Jan Ouwens <jan.ouwens@gmail.com>
2020-07-09 09:58:43 +0200

    Adds Hello World demo

---
+++ src/main/java/nl/jqno/annotationscript/demo/HelloWorld.java
@@ -0,0 +1,12 @@
+package nl.jqno.annotationscript.demo;
+
+import nl.jqno.annotationscript.AnnotationScript;
+import nl.jqno.annotationscript.Annotations.Zero;
+
+@Zero("println")
+@Zero("'Hello world!'")
+public class HelloWorld {
+    public static void main(String[] args) {
+        AnnotationScript.run(HelloWorld.class);
+    }
+}
