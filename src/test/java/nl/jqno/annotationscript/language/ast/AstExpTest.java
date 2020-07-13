16f69e122a0a081b6f6a18268e6ecdc8feeb656e
Jan Ouwens <jan.ouwens@gmail.com>
2020-07-13 20:39:32 +0200

    Removes unused import

--- src/test/java/nl/jqno/annotationscript/language/ast/AstExpTest.java
+++ src/test/java/nl/jqno/annotationscript/language/ast/AstExpTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
-import nl.jqno.annotationscript.language.ast.AstExpTest.AstBogus;
 import nl.jqno.annotationscript.language.exceptions.EvaluationException;
 import nl.jqno.equalsverifier.EqualsVerifier;
 
