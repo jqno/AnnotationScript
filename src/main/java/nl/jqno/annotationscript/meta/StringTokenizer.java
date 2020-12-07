92bb65b7c52a26ebc777b20848a4f9632b894720
Jan Ouwens <jan.ouwens@gmail.com>
2020-12-07 16:18:04 +0100

    Fixes strings-containing-spaces issue in Meta-tokenizer

--- src/main/java/nl/jqno/annotationscript/meta/StringTokenizer.java
+++ src/main/java/nl/jqno/annotationscript/meta/StringTokenizer.java
@@ -2,10 +2,25 @@ package nl.jqno.annotationscript.meta;
 
 import nl.jqno.annotationscript.Annotations.*;
 
-@Zero("filter")
-@Zero(list={@One("lambda"), @One(list={@Two("s")}), @One(list={@Two("not"), @Two(list={@Three("="), @Three("s"), @Three("''")})})})
-@Zero(list={@One("str/split"), @One("' '"), @One(list={
-    @Two("str/replace"), @Two(list={
-        @Three("str/replace"), @Three("input"), @Three("'('"), @Three("' ( '")}),
-    @Two("')'"), @Two("' ) '")})})
+@Zero("begin")
+@Zero(list={@One("define"), @One("PUNCTUATION_SPACE"), @One("'\u2008'")})
+@Zero(list={
+    @One("filter"),
+    @One(list={
+        @Two("lambda"),
+        @Two(list={@Three("s")}),
+        @Two(list={@Three("not"), @Three(list={@Four("="), @Four("s"), @Four("''")})})}),
+    @One(list={
+        @Two("map"),
+        @Two(list={
+            @Three("lambda"),
+            @Three(list={@Four("s")}),
+            @Three(list={@Four("str/replace"), @Four("s"), @Four("PUNCTUATION_SPACE"), @Four("' '")})}),
+        @Two(list={
+            @Three("str/split"), @Three("' '"), @Three(list={
+                @Four("str/replace"), @Four(list={
+                    @Five("str/replace"), @Five(list={
+                        @Six("str/replace"), @Six("input"), @Six("'\\ '"), @Six("PUNCTUATION_SPACE")}),
+                    @Five("'('"), @Five("' ( '")}),
+                @Four("')'"), @Four("' ) '")})})})})
 public class StringTokenizer {}
