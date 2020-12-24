8806dc7c3bea76d04ab5593e313abfc3085edd2f
Jan Ouwens <jan.ouwens@gmail.com>
2020-12-24 15:28:03 +0100

    Adds meta-FizzBuzz demo

---
+++ src/main/java/nl/jqno/annotationscript/demo/MetaFizzBuzz.java
@@ -0,0 +1,27 @@
+package nl.jqno.annotationscript.demo;
+
+import nl.jqno.annotationscript.meta.MetaScript;
+
+public class MetaFizzBuzz {
+    public static void main(String[] args) {
+        var fizzbuzz =
+            "(define" +
+            "  (fizzbuzz" +
+            "    (lambda (n)" +
+            "      (cond" +
+            "        ((eq? 0 (mod n 15)) (quote fizzbuzz))" +
+            "        ((eq? 0 (mod n 3)) (quote fizz))" +
+            "        ((eq? 0 (mod n 5)) (quote buzz))" +
+            "        (else n))))" +
+            "  (define" +
+            "    (run" +
+            "      (lambda (i end recurse)" +
+            "        (cond" +
+            "          ((eq? i end) (quote ()))" +
+            "          (else" +
+            "            (cons (fizzbuzz i) (recurse (add1 i) end recurse))))))" +
+            "    (run 1 31 run)))";
+        var output = MetaScript.run(fizzbuzz);
+        System.out.println(output); // CHECKSTYLE OFF: Regexp
+    }
+}
