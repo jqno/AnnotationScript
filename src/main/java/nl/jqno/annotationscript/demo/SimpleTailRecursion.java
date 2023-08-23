d3d7696f229f69289d2c4aea282fd0d21930746f
Jan Ouwens <jan.ouwens@gmail.com>
2023-08-23 21:02:25 +0200

    Adds SimpleTailRecursion demo

---
+++ src/main/java/nl/jqno/annotationscript/demo/SimpleTailRecursion.java
@@ -0,0 +1,36 @@
+//REPOS jitpack
+//DEPS com.github.jqno:AnnotationScript:main-SNAPSHOT
+package nl.jqno.annotationscript.demo;
+
+import nl.jqno.annotationscript.AnnotationScript;
+import nl.jqno.annotationscript.Annotations.Five;
+import nl.jqno.annotationscript.Annotations.Four;
+import nl.jqno.annotationscript.Annotations.One;
+import nl.jqno.annotationscript.Annotations.Three;
+import nl.jqno.annotationscript.Annotations.Two;
+import nl.jqno.annotationscript.Annotations.Zero;
+
+// Without tail call optimization, this demo throws a StackOverflowError after ~900 iterations on my machine.
+// With tail call optimization, it can run arbitrarily long. Try it!
+
+@Zero("begin")
+@Zero(list={
+    @One("define"),
+    @One("loop"),
+    @One(list={
+        @Two("lambda"),
+        @Two(list={@Three("n"), @Three("count")}),
+        @Two(list={
+            @Three("if"),
+            @Three(list={@Four("="), @Four("count"), @Four("n")}),
+            @Three("n"),
+            @Three(list={
+                @Four("loop"),
+                @Four("n"),
+                @Four(list={@Five("+"), @Five("count"), @Five("1")})})})})})
+@Zero(list={@One("println"), @One(list={@Two("loop"), @Two("1000"), @Two("0")})})
+public class SimpleTailRecursion {
+    public static void main(String[] args) {
+        AnnotationScript.run(SimpleTailRecursion.class);
+    }
+}
