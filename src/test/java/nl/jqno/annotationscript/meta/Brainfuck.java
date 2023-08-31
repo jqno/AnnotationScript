0c4b8c2bc0b106e71e00d790c4de36ec85c6e24a
Jan Ouwens <jan.ouwens@gmail.com>
2023-08-31 08:24:21 +0200

    Brainfuck: stack-overflowing interpreter

--- src/test/java/nl/jqno/annotationscript/meta/Brainfuck.java
+++ src/test/java/nl/jqno/annotationscript/meta/Brainfuck.java
@@ -9,10 +9,8 @@ public class Brainfuck {
     @Test
     public void main() {
         var brainfuck = """
-            (define (hello-world (quote (
-                > + + + + + + + + + [ < + + + + + + + + > - ] < . > + + + + + + + [ < + + + + > - ] < + . + + + + + + + . . + + + . > > > +
-                + + + + + + + [ < + + + + > - ] < . > > > + + + + + + + + + + [ < + + + + + + + + + > - ] < - - - . < < < < . + + + . - - -
-                - - - . - - - - - - - - . > > + . > + + + + + + + + + + .
+            (define (program (quote (
+                + + + + + + [ - > + + + + + + < ] > .
             )))
 
             (define (fold-left
@@ -59,6 +57,46 @@ public class Brainfuck {
               (lambda (tape pointer program-counter output stack)
                 (cons tape (cons pointer (cons program-counter (cons output (cons stack (quote ()))))))))
 
+            (define (bf-interpreter
+              (lambda (program)
+                (define (final-state
+                  (fold-left
+                    (lambda (state cmd)
+                      (define (tape (car state))
+                      (define (pointer (car (cdr state)))
+                      (define (program-counter (car (cdr (cdr state))))
+                      (define (output (car (cdr (cdr (cdr state)))))
+                      (define (stack (car (cdr (cdr (cdr (cdr state))))))
+
+                      (cond
+                        ((eq? cmd '>')
+                         (create-state tape (+ pointer 1) (+ program-counter 1) output stack))
+                        ((eq? cmd '<')
+                         (create-state tape (- pointer 1) (+ program-counter 1) output stack))
+                        ((eq? cmd '+')
+                         (create-state (update-nth pointer (+ 1 (nth pointer tape)) tape) pointer (+ program-counter 1) output stack))
+                        ((eq? cmd '-')
+                         (create-state (update-nth pointer (- (nth pointer tape) 1) tape) pointer (+ program-counter 1) output stack))
+                        ((eq? cmd '.')
+                         (create-state tape pointer (+ program-counter 1) (cons (nth pointer tape) output) stack))
+                        ((eq? cmd '[')
+                         (cond
+                           ((zero? (nth pointer tape))
+                            (create-state tape pointer (nth (car stack) stack) output (cdr stack)))
+                           (else
+                            (create-state tape pointer (+ program-counter 1) output (cons program-counter stack)))))
+                        ((eq? cmd ']')
+                         (cond
+                           ((zero? (nth pointer tape))
+                            (create-state tape pointer (+ program-counter 1) output (cdr stack)))
+                           (else
+                            (create-state tape pointer (car stack) output stack))))
+                        (else
+                         state))))))))
+                    (create-state tape 0 0 (quote ()) (quote ()))
+                    program)))
+                (car (cdr (cdr (cdr final-state))))))
+
             (define (tape (quote (
                 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
                 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
@@ -82,7 +120,7 @@ public class Brainfuck {
                 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
             )))
 
-            (update-nth 2 1337 (quote (1 2 3 4 5))))))))))
+            (bf-interpreter program)))))))))
             """;
         var output = MetaScript.run(brainfuck);
         System.out.println(output); // CHECKSTYLE OFF: Regexp
